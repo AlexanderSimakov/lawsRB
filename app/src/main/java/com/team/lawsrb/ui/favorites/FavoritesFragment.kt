7a4ecbf6ff62b559694aeb635059c878ccdf0862
Alexander <casha4367@mail.ru>
2022-07-05 22:19:56 +0300

    Add favorites fragment, view model and layout

---
+++ app/src/main/java/com/team/lawsrb/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,46 @@
+package com.team.lawsrb.ui.favorites
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.team.lawsrb.R
+import com.team.lawsrb.databinding.FragmentFavoritesBinding
+import com.team.lawsrb.databinding.FragmentGalleryBinding
+import com.team.lawsrb.ui.gallery.GalleryViewModel
+
+class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val favoritesViewModel =
+            ViewModelProvider(this).get(FavoritesViewModel::class.java)
+
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.exampleText
+        favoritesViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
