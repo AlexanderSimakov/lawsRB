a118b7213c424e594d88059a42090018b4e06287
Alexander <casha4367@mail.ru>
2022-07-19 17:10:26 +0300

    Use ObjectFragments from codeObjectFragments

--- app/src/main/java/com/team/lawsrb/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/team/lawsrb/ui/favorites/FavoritesFragment.kt
@@ -10,6 +10,9 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.team.lawsrb.R
+import com.team.lawsrb.ui.codeObjectFragments.ArticleObjectFragment
+import com.team.lawsrb.ui.codeObjectFragments.ChapterObjectFragment
+import com.team.lawsrb.ui.codeObjectFragments.SectionObjectFragment
 
 class FavoritesFragment : Fragment() {
     // When requested, this adapter returns a DemoObjectFragment,
@@ -45,39 +48,10 @@ class DemoCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment)
     override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
-        // Return a NEW fragment instance in createFragment(int)
-        val fragment = DemoObjectFragment()
-        fragment.arguments = Bundle().apply {
-            // Just for example, to change page text
-            when (position){
-                0 -> putString(ARG_OBJECT, "Разделы")
-                1 -> putString(ARG_OBJECT, "Главы")
-                else -> putString(ARG_OBJECT, "Статьи")
-            }
-        }
-        return fragment
-    }
-}
-
-private const val ARG_OBJECT = "object"
-
-// Instances of this class are fragments representing a single
-// object in our collection.
-class DemoObjectFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
-            // Just for example
-            val textView: TextView = view.findViewById(R.id.example_text)
-            textView.text = getString(ARG_OBJECT)
+        return when (position){
+            0 -> SectionObjectFragment()
+            1 -> ChapterObjectFragment()
+            else -> ArticleObjectFragment()
         }
     }
 }
