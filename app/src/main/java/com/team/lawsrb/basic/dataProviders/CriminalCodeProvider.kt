d4971fa2d2ca9e16c28c7141e58d1d57cf0f209e
Alexander <casha4367@mail.ru>
2022-07-21 21:13:14 +0300

    Fill CriminalCodeProvider with test code

--- app/src/main/java/com/team/lawsrb/basic/dataProviders/CriminalCodeProvider.kt
+++ app/src/main/java/com/team/lawsrb/basic/dataProviders/CriminalCodeProvider.kt
@@ -1,4 +1,49 @@
 package com.team.lawsrb.basic.dataProviders
 
-class CriminalCodeProvider {
+import com.team.lawsrb.basic.codeObjects.Article
+import com.team.lawsrb.basic.codeObjects.Chapter
+import com.team.lawsrb.basic.codeObjects.Part
+import com.team.lawsrb.basic.codeObjects.Section
+
+object CriminalCodeProvider {
+    private val parts = mutableListOf<Part>()
+    private val sections = mutableListOf<Section>()
+    private val chapters = mutableListOf<Chapter>()
+    private val articles = mutableListOf<Article>()
+
+    init {
+        for (id in 0..1) parts.add(Part(id, "Part ${id+1}"))
+        for (id in 0..5) sections.add(Section(id, if (id > 3) 1 else 0, "Section ${id+1}"))
+        for (id in 0..13) chapters.add(Chapter(id, (id-1)/2, "Chapter ${id+1}"))
+        for (id in 0..27) articles.add(Article(id, (id-1)/2, "Article ${id+1}"))
+    }
+
+    fun getParts() = parts
+    fun getSections() = sections
+    fun getChapters() = chapters
+    fun getArticles() = articles
+
+    fun getSections(part: Part): MutableList<Section>{
+        val sections = mutableListOf<Section>()
+        for (section in this.sections){
+            if (section.partId == part.id) sections.add(section)
+        }
+        return sections
+    }
+
+    fun getChapters(section: Section): MutableList<Chapter>{
+        val chapters = mutableListOf<Chapter>()
+        for (chapter in this.chapters){
+            if (chapter.sectionId == section.id) chapters.add(chapter)
+        }
+        return chapters
+    }
+
+    fun getArticles(chapter: Chapter): MutableList<Article>{
+        val articles = mutableListOf<Article>()
+        for (article in this.articles){
+            if (article.chapterId == chapter.id) articles.add(article)
+        }
+        return articles
+    }
 }
