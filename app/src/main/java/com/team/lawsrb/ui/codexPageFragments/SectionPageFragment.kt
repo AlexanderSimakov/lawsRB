71af91cbffb3df0b2a5d58f0ea99580c0a3f0f88
Alexander <casha4367@mail.ru>
2022-08-04 22:06:31 +0300

    Add RecyclerView for SectionPageFragment

--- app/src/main/java/com/team/lawsrb/ui/codexPageFragments/SectionPageFragment.kt
+++ app/src/main/java/com/team/lawsrb/ui/codexPageFragments/SectionPageFragment.kt
@@ -1,22 +1,23 @@
 package com.team.lawsrb.ui.codexPageFragments
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.ScrollView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.team.lawsrb.R
+import com.team.lawsrb.basic.codexObjects.Chapter
+import com.team.lawsrb.basic.codexObjects.Part
 import com.team.lawsrb.basic.codexObjects.Section
 import com.team.lawsrb.basic.dataProviders.CodexProvider
-import com.team.lawsrb.ui.informationViewers.CardViewFactory
 
 class SectionPageFragment(private val codeProvider: CodexProvider,
                           private val pager_id: Int) : Fragment() {
     //private lateinit var layout: LinearLayout
+    private var items: MutableList<Any> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +28,12 @@ class SectionPageFragment(private val codeProvider: CodexProvider,
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initItems()
+
+        val rvSections = view.findViewById<View>(R.id.code_viewer_fragment_recycler_view) as RecyclerView
+        rvSections.adapter = SectionsPageAdapter(items)
+        rvSections.layoutManager = LinearLayoutManager(context)
+
         /*
         Scrollable.view = view
         layout = view.findViewById<LinearLayout>(R.id.code_viewer_fragment_content)
@@ -36,6 +43,13 @@ class SectionPageFragment(private val codeProvider: CodexProvider,
             codeProvider.getSections(part).forEach { addSectionToLayout(it) }
         }
          */
+    }
+
+    private fun initItems(){
+        for (part in codeProvider.getParts()){
+            items.add(part)
+            codeProvider.getSections(part).forEach { items.add(it) }
+        }
     }
 
     /*
@@ -70,4 +84,59 @@ class SectionPageFragment(private val codeProvider: CodexProvider,
         }
     }
     */
+}
+
+class SectionsPageAdapter (private val items: List<Any>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val isSection = 1
+    private val isPart = 2
+
+    inner class PartViewHolder(partCardView: View) : RecyclerView.ViewHolder(partCardView) {
+        val title: TextView = partCardView.findViewById(R.id.light_card_title)
+        val content: TextView = partCardView.findViewById(R.id.light_card_content)
+    }
+
+    inner class SectionViewHolder(sectionCardView: View) : RecyclerView.ViewHolder(sectionCardView) {
+        val title: TextView = sectionCardView.findViewById(R.id.dark_card_title)
+        val content: TextView = sectionCardView.findViewById(R.id.dark_card_content)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (items[position]::class){
+            Section::class -> isSection
+            Part::class -> isPart
+            else -> throw IllegalArgumentException("items: List<Any> contain class ${items[position]::class.simpleName}, expected Section or Part")
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            isSection -> {
+                val lightCardView = inflater.inflate(R.layout.dark_card, parent, false)
+                SectionViewHolder(lightCardView)
+            }
+            isPart -> {
+                val lightCardView = inflater.inflate(R.layout.light_card, parent, false)
+                PartViewHolder(lightCardView)
+            }
+            else -> throw IllegalArgumentException("viewType was $viewType, expected $isSection or $isPart")
+        }
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) =
+        when (viewHolder.itemViewType){
+            isSection -> {
+                val section: Section = items[position] as Section
+                (viewHolder as SectionViewHolder).title.text = section.title
+                viewHolder.content.text = "Section content"
+            }
+            isPart -> {
+                val part: Part = items[position] as Part
+                (viewHolder as PartViewHolder).title.text = part.title
+                viewHolder.content.text = "Part content"
+            }
+            else -> throw IllegalArgumentException("itemViewType was ${viewHolder.itemViewType}, expected $isSection or $isPart")
+        }
 }
