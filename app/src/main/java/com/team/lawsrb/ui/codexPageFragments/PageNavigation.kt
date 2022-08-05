5f238061160d80c9c90633e09c9cea9fe20eb62f
Alexander <casha4367@mail.ru>
2022-08-05 21:38:39 +0300

    Fix bug

--- app/src/main/java/com/team/lawsrb/ui/codexPageFragments/PageNavigation.kt
+++ app/src/main/java/com/team/lawsrb/ui/codexPageFragments/PageNavigation.kt
@@ -2,12 +2,15 @@ package com.team.lawsrb.ui.codexPageFragments
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import java.util.*
+import kotlin.concurrent.schedule
 
 object PageNavigation {
     var viewPager: ViewPager2? = null
 
     private class Recycler(val recyclerView: RecyclerView, val items: MutableList<Any>)
     private var recyclers: MutableList<Recycler> = mutableListOf()
+    private val delayBeforeScrolling = 300L
 
     fun addRecyclerView(view: RecyclerView, items: MutableList<Any>, number: Int) {
         recyclers.add(number, Recycler(view, items))
@@ -15,15 +18,19 @@ object PageNavigation {
 
     fun moveLeftTo(codexObject: Any) {
         viewPager!!.setCurrentItem(viewPager!!.currentItem - 1, true)
-        adjustPosition(codexObject)
+        Timer().schedule(delayBeforeScrolling){
+            scrollPageTo(codexObject)
+        }
     }
 
     fun moveRightTo(codexObject: Any) {
         viewPager!!.setCurrentItem(viewPager!!.currentItem + 1, true)
-        adjustPosition(codexObject)
+        Timer().schedule(delayBeforeScrolling){
+            scrollPageTo(codexObject)
+        }
     }
 
-    private fun adjustPosition(codexObject: Any){
+    private fun scrollPageTo(codexObject: Any){
         recyclers[viewPager!!.currentItem].let { recycler ->
             val position = recycler.items.indexOfFirst { it == codexObject }
             recycler.recyclerView.smoothScrollToPosition(position)
