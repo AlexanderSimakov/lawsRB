d36c8a0dd8b3c155f4b223c24451e9321c6b25fa
Alexander <casha4367@mail.ru>
2022-08-25 17:24:11 +0300

    feat(settings): add simple update buttons

--- app/src/main/java/com/team/lawsrb/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/team/lawsrb/ui/settings/SettingsFragment.kt
@@ -1,13 +1,14 @@
 package com.team.lawsrb.ui.settings
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import com.team.lawsrb.R
 import com.team.lawsrb.databinding.FragmentSettingsBinding
+import kotlinx.android.synthetic.main.fragment_settings.view.*
+import kotlinx.android.synthetic.main.update_codex_button.view.*
 
 class SettingsFragment : Fragment() {
 
@@ -26,7 +27,21 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setUpUpdateButtons()
+
         return root
+    }
+
+    private fun setUpUpdateButtons(){
+        binding.settingsFragment.update_uk.title.text = getString(R.string.menu_criminal_code)
+        binding.settingsFragment.update_upk.title.text = getString(R.string.menu_code_of_criminal_proсedure)
+        binding.settingsFragment.update_koap.title.text = getString(R.string.menu_KoAP)
+        binding.settingsFragment.update_pikoap.title.text = getString(R.string.menu_PIKoAP)
+
+        binding.settingsFragment.update_uk.subtitle.text = "От 31.12.1979"
+        binding.settingsFragment.update_upk.subtitle.text = "От 31.12.1979"
+        binding.settingsFragment.update_koap.subtitle.text = "От 31.12.1979"
+        binding.settingsFragment.update_pikoap.subtitle.text = "От 31.12.1979"
     }
 
     override fun onDestroyView() {
