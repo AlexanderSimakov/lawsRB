a7ad073b11fb4b16249b05de0818ebbf1b4ef32e
Alexander <casha4367@mail.ru>
2022-08-25 14:34:27 +0300

    feat: add debug code

--- app/src/main/java/com/team/lawsrb/MainActivity.kt
+++ app/src/main/java/com/team/lawsrb/MainActivity.kt
@@ -25,8 +25,13 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.team.lawsrb.basic.dataProviders.*
+import com.team.lawsrb.basic.htmlParser.Codex
+import com.team.lawsrb.basic.htmlParser.CodexLists
+import com.team.lawsrb.basic.htmlParser.Parser
 import com.team.lawsrb.basic.roomDatabase.*
 import com.team.lawsrb.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -70,7 +75,77 @@ class MainActivity : AppCompatActivity() {
 
         //Initialize database
         BaseCodexDatabase.init(applicationContext)
+
+        /*
+        clearAll()
+
+        GlobalScope.launch {
+            Log.i("MainActivity", "Start parsing UK")
+            val allUK: CodexLists = Parser.get(Codex.UK)
+            Log.i("MainActivity", allUK.toString())
+            Log.i("MainActivity", "Start adding UK")
+            allUK.parts.forEach { BaseCodexDatabase.UK.partsDao().insert(it) }
+            allUK.sections.forEach { BaseCodexDatabase.UK.sectionsDao().insert(it) }
+            allUK.chapters.forEach { BaseCodexDatabase.UK.chaptersDao().insert(it) }
+            allUK.articles.forEach { BaseCodexDatabase.UK.articlesDao().insert(it) }
+            Log.i("MainActivity", "End with UK")
+
+            Log.i("MainActivity", "Start parsing UPK")
+            val allUPK: CodexLists = Parser.get(Codex.UPK)
+            Log.i("MainActivity", allUPK.toString())
+            Log.i("MainActivity", "Start adding UPK")
+            allUPK.parts.forEach { BaseCodexDatabase.UPK.partsDao().insert(it) }
+            allUPK.sections.forEach { BaseCodexDatabase.UPK.sectionsDao().insert(it) }
+            allUPK.chapters.forEach { BaseCodexDatabase.UPK.chaptersDao().insert(it) }
+            allUPK.articles.forEach { BaseCodexDatabase.UPK.articlesDao().insert(it) }
+            Log.i("MainActivity", "End with UPK")
+
+            Log.i("MainActivity", "Start parsing KoAP")
+            val allKoAP: CodexLists = Parser.get(Codex.KoAP)
+            Log.i("MainActivity", "Start adding KoAP")
+            allKoAP.parts.forEach { BaseCodexDatabase.KoAP.partsDao().insert(it) }
+            allKoAP.sections.forEach { BaseCodexDatabase.KoAP.sectionsDao().insert(it) }
+            allKoAP.chapters.forEach { BaseCodexDatabase.KoAP.chaptersDao().insert(it) }
+            allKoAP.articles.forEach { BaseCodexDatabase.KoAP.articlesDao().insert(it) }
+            Log.i("MainActivity", "End with KoAP")
+
+            Log.i("MainActivity", "Start parsing PIKoAP")
+            val allPIKoAP: CodexLists = Parser.get(Codex.PIKoAP)
+            Log.i("MainActivity", "Start adding PIKoAP")
+            allPIKoAP.parts.forEach { BaseCodexDatabase.PIKoAP.partsDao().insert(it) }
+            allPIKoAP.sections.forEach { BaseCodexDatabase.PIKoAP.sectionsDao().insert(it) }
+            allPIKoAP.chapters.forEach { BaseCodexDatabase.PIKoAP.chaptersDao().insert(it) }
+            allPIKoAP.articles.forEach { BaseCodexDatabase.PIKoAP.articlesDao().insert(it) }
+            Log.i("MainActivity", "End with PIKoAP")
+        }
+        */
     }
+
+    /*
+    private fun clearAll(){
+        Log.i("MainActivity", "Clear all start")
+        BaseCodexDatabase.UK.articlesDao().clearAll()
+        BaseCodexDatabase.UK.chaptersDao().clearAll()
+        BaseCodexDatabase.UK.sectionsDao().clearAll()
+        BaseCodexDatabase.UK.partsDao().clearAll()
+
+        BaseCodexDatabase.UPK.articlesDao().clearAll()
+        BaseCodexDatabase.UPK.chaptersDao().clearAll()
+        BaseCodexDatabase.UPK.sectionsDao().clearAll()
+        BaseCodexDatabase.UPK.partsDao().clearAll()
+
+        BaseCodexDatabase.KoAP.articlesDao().clearAll()
+        BaseCodexDatabase.KoAP.chaptersDao().clearAll()
+        BaseCodexDatabase.KoAP.sectionsDao().clearAll()
+        BaseCodexDatabase.KoAP.partsDao().clearAll()
+
+        BaseCodexDatabase.PIKoAP.articlesDao().clearAll()
+        BaseCodexDatabase.PIKoAP.chaptersDao().clearAll()
+        BaseCodexDatabase.PIKoAP.sectionsDao().clearAll()
+        BaseCodexDatabase.PIKoAP.partsDao().clearAll()
+        Log.i("MainActivity", "Clear all end")
+    }
+    */
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
