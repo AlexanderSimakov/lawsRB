f7fc0b847a6e116e8feb37b7547c36f3d078fecc
Alexander <casha4367@mail.ru>
2022-09-01 23:06:51 +0300

    fix(parser): add method call

--- app/src/main/java/com/team/lawsrb/basic/htmlParser/Parser.kt
+++ app/src/main/java/com/team/lawsrb/basic/htmlParser/Parser.kt
@@ -26,6 +26,7 @@ class Parser
         contentList.clear()
         articlesList.clear()
 
+        parsePartsTitles()
         parseSectionsTitles()
         parseChaptersTitles()
         parseArticlesTitles()
