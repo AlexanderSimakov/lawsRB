3a2817498fd32c268579d8089393ead2c9df53e6
Alexander <casha4367@mail.ru>
2022-09-02 22:06:13 +0300

    refact(scroll): minor changes
    1. move CenterSmoothScroller to separate class
    2. override calculateTimeForDelceleration(dx: Int)
    3. adjust scroll speed

--- app/src/main/java/com/team/lawsrb/ui/codexPageFragments/CenterLayoutManager.kt
+++ app/src/main/java/com/team/lawsrb/ui/codexPageFragments/CenterLayoutManager.kt
@@ -3,6 +3,7 @@ package com.team.lawsrb.ui.codexPageFragments
 import android.content.Context
 import android.util.AttributeSet
 import android.util.DisplayMetrics
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
@@ -26,17 +27,20 @@ class CenterLayoutManager : LinearLayoutManager {
         startSmoothScroll(smoothScroller)
     }
 
-    companion object{
-        class CenterSmoothScroller(context: Context) : LinearSmoothScroller(context) {
+}
 
-            override fun getVerticalSnapPreference() = SNAP_TO_START
-
-            override fun calculateTimeForScrolling(dx: Int) =
-                when (dx){
-                    10_000 -> 100
-                    else -> 300
-                }
-        }
+class CenterSmoothScroller(context: Context) : LinearSmoothScroller(context) {
+    companion object {
+        private const val MILLISECONDS_PER_INCH = 5F
     }
 
+    override fun getVerticalSnapPreference() = SNAP_TO_START
+
+    override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics?): Float {
+        return MILLISECONDS_PER_INCH / displayMetrics!!.densityDpi
+    }
+
+    override fun calculateTimeForDeceleration(dx: Int): Int {
+        return super.calculateTimeForDeceleration(dx) * 5
+    }
 }
