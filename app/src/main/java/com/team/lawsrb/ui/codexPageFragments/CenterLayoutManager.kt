f740cff6bf99a27c2fe92bd9a2746a16a9c9a5b5
Alexander Simakov <50450591+AlexanderSimakov@users.noreply.github.com>
2022-09-02 22:08:50 +0300

    Squash merge  'adjust-scroll' to 'dev' (#30)
    * refact(scroll): use another function
    
    * refact(scroll): remove super. call
    
    * refact(scroll): adjust code style
    
    * refact(scroll): use another function
    
    * fix(scroll): remove bracket
    
    * refact(scroll): minor changes
    
    1. move CenterSmoothScroller to separate class
    2. override calculateTimeForDelceleration(dx: Int)
    3. adjust scroll speed

--- app/src/main/java/com/team/lawsrb/ui/codexPageFragments/CenterLayoutManager.kt
+++ app/src/main/java/com/team/lawsrb/ui/codexPageFragments/CenterLayoutManager.kt
@@ -3,6 +3,7 @@ package com.team.lawsrb.ui.codexPageFragments
 import android.content.Context
 import android.util.AttributeSet
 import android.util.DisplayMetrics
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
@@ -21,33 +22,25 @@ class CenterLayoutManager : LinearLayoutManager {
         state: RecyclerView.State?,
         position: Int
     ) {
-        super.smoothScrollToPosition(recyclerView, state, position)
         val smoothScroller = CenterSmoothScroller(recyclerView!!.context)
         smoothScroller.targetPosition = position
         startSmoothScroll(smoothScroller)
     }
 
-    companion object{
-        class CenterSmoothScroller(context: Context) : LinearSmoothScroller(context) {
-            private val millisecondsPerInch = 40F // default is 25 (bigger = slower)
+}
 
-            override fun calculateDtToFit(
-                viewStart: Int,
-                viewEnd: Int,
-                boxStart: Int,
-                boxEnd: Int,
-                snapPreference: Int
-            ): Int {
-                return super.calculateDtToFit(viewStart, viewEnd, boxStart, boxEnd, SNAP_TO_START)
-            }
-
-            override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics?): Float {
-                if (displayMetrics != null) {
-                    return millisecondsPerInch / displayMetrics.densityDpi
-                }
-                return 4F
-            }
-        }
+class CenterSmoothScroller(context: Context) : LinearSmoothScroller(context) {
+    companion object {
+        private const val MILLISECONDS_PER_INCH = 5F
     }
 
+    override fun getVerticalSnapPreference() = SNAP_TO_START
+
+    override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics?): Float {
+        return MILLISECONDS_PER_INCH / displayMetrics!!.densityDpi
+    }
+
+    override fun calculateTimeForDeceleration(dx: Int): Int {
+        return super.calculateTimeForDeceleration(dx) * 5
+    }
 }
