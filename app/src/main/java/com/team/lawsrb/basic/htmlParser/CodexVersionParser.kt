b87e359e22e0144d8ebda3e9d4d794e7dbac8274
tokyolem <heenssss@mail.ru>
2022-09-04 00:35:41 +0300

    feat(CodexVersionsParser): added parser of code versions

---
+++ app/src/main/java/com/team/lawsrb/basic/htmlParser/CodexVersionParser.kt
@@ -0,0 +1,56 @@
+package com.team.lawsrb.basic.htmlParser
+
+import android.util.Log
+import com.team.lawsrb.MainActivity
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+object CodexVersionParser {
+
+    private var document: Document? = null
+
+    fun get(codex: Codex): Boolean{
+        try
+        {
+            document = Jsoup.connect(codex.URL).get()
+        }
+        catch (e: Exception)
+        {
+            Log.e("Error", "Failed getting document")
+        }
+
+        val countOfElements = getQuantityOfElementsWithChanges()
+        val sharedPrefCodexVersions = MainActivity.mSettingCodexVersions
+        var oldCountOfElements = 0
+        if (sharedPrefCodexVersions.contains(codex.name))
+        {
+            oldCountOfElements = sharedPrefCodexVersions.getInt(codex.name, countOfElements)
+        }
+
+        Log.d("TestLog", "$oldCountOfElements")
+        if (countOfElements != oldCountOfElements)
+        {
+            sharedPrefCodexVersions.edit().remove(codex.name).apply()
+            sharedPrefCodexVersions.edit().putInt(codex.name, countOfElements).apply()
+            return true
+        }
+
+        return false
+    }
+
+    private fun getQuantityOfElementsWithChanges(): Int
+    {
+        var quantityOfElements = 0
+        val mainTable = document!!.select("main")
+        val elements = mainTable.select("p")
+
+        for (element in elements)
+        {
+            if (element.attr("class").equals("changeadd"))
+            {
+                quantityOfElements++
+            }
+        }
+        return quantityOfElements
+    }
+}
