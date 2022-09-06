da1d73797e2c8ba11f5cc42b454d2da658814d3a
Alexander <casha4367@mail.ru>
2022-09-06 20:22:46 +0300

    feat(preferences): add Preferences object

---
+++ app/src/main/java/com/team/lawsrb/basic/Preferences.kt
@@ -0,0 +1,76 @@
+package com.team.lawsrb.basic
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.team.lawsrb.basic.htmlParser.Codex
+
+object Preferences {
+
+    private const val PREFERENCES_NAME = "com.team.lawsrb.preferences"
+    private var sharedPref: SharedPreferences? = null
+
+    // keys
+    private const val IS_DARK_THEME_KEY = "isDarkModeOn"
+    private const val IS_RUN_FIRST_KEY = "isRunFirst"
+    private const val CODEX_VERSION_AFFIX = "version"
+    private const val CODEX_UPDATE_DATE_AFFIX = "updateDate"
+
+    fun update(context: Context){
+        sharedPref = context.getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE)
+    }
+
+    var isDarkTheme: Boolean
+        get() {
+            return sharedPref!!.getBoolean(IS_DARK_THEME_KEY, false)
+        }
+        set(value) {
+            sharedPref!!.edit().apply {
+                putBoolean(IS_DARK_THEME_KEY, value)
+                apply()
+            }
+        }
+
+    var isRunFirst: Boolean
+        get() {
+            return !sharedPref!!.contains(IS_RUN_FIRST_KEY)
+        }
+        set(value){
+            if (isRunFirst){
+                sharedPref?.edit()?.apply {
+                    putBoolean(IS_RUN_FIRST_KEY, value)
+                    apply()
+                }
+            }
+        }
+
+    fun setCodexVersion(codex: Codex, version: Int){
+        sharedPref!!.edit().apply {
+            putInt(CODEX_VERSION_AFFIX + codex.name, version)
+            apply()
+        }
+    }
+
+    fun setCodexUpdateDate(codex: Codex, date: String){
+        sharedPref!!.edit().apply {
+            putString(CODEX_UPDATE_DATE_AFFIX + codex.name, date)
+            apply()
+        }
+    }
+
+    fun setCodexInfo(codex: Codex, version: Int, date: String){
+        sharedPref!!.edit().apply {
+            putInt(CODEX_VERSION_AFFIX + codex.name, version)
+            putString(CODEX_UPDATE_DATE_AFFIX + codex.name, date)
+            apply()
+        }
+    }
+
+    fun getCodexVersion(codex: Codex): Int{
+        return sharedPref!!.getInt(CODEX_VERSION_AFFIX + codex.name, -1)
+    }
+
+    fun getCodexUpdateDate(codex: Codex): String{
+        return sharedPref!!.getString(CODEX_UPDATE_DATE_AFFIX + codex.name, "")!!
+    }
+}
