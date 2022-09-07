a8a07f3a1391e3a014cff48f0fa3d730fe2f90ec
Alexander <casha4367@mail.ru>
2022-09-07 22:49:55 +0300

    refact(settings): improve code readability

--- app/src/main/java/com/team/lawsrb/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/team/lawsrb/ui/settings/SettingsFragment.kt
@@ -18,6 +18,7 @@ import android.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.team.lawsrb.basic.roomDatabase.CodexDatabase
 import com.team.lawsrb.databinding.FragmentSettingsBinding
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 import kotlinx.android.synthetic.main.update_codex_button.view.*
@@ -62,15 +63,17 @@ class SettingsFragment : Fragment() {
     }
 
     private fun setUpUpdateButtons(){
-        binding.settingsFragment.update_uk.title.text = getString(R.string.menu_criminal_code)
-        binding.settingsFragment.update_upk.title.text = getString(R.string.menu_code_of_criminal_proсedure)
-        binding.settingsFragment.update_koap.title.text = getString(R.string.menu_KoAP)
-        binding.settingsFragment.update_pikoap.title.text = getString(R.string.menu_PIKoAP)
+        binding.settingsFragment.apply {
+            update_uk.title.text = getString(R.string.menu_criminal_code)
+            update_upk.title.text = getString(R.string.menu_code_of_criminal_proсedure)
+            update_koap.title.text = getString(R.string.menu_KoAP)
+            update_pikoap.title.text = getString(R.string.menu_PIKoAP)
 
-        binding.settingsFragment.update_uk.subtitle.text = "От 31.12.1979"
-        binding.settingsFragment.update_upk.subtitle.text = "От 31.12.1979"
-        binding.settingsFragment.update_koap.subtitle.text = "От 31.12.1979"
-        binding.settingsFragment.update_pikoap.subtitle.text = "От 31.12.1979"
+            update_uk.subtitle.text = "От 31.12.1979"
+            update_upk.subtitle.text = "От 31.12.1979"
+            update_koap.subtitle.text = "От 31.12.1979"
+            update_pikoap.subtitle.text = "От 31.12.1979"
+        }
     }
 
     private fun setOnClickListenerForUpdateButtons(){
@@ -198,30 +201,23 @@ class SettingsFragment : Fragment() {
     // debug function
     private fun setUpClearAllButton(){
         binding.settingsFragment.debug_clear_all_button.setOnClickListener {
-            BaseCodexDatabase.UK.articlesDao().clearAll()
-            BaseCodexDatabase.UK.chaptersDao().clearAll()
-            BaseCodexDatabase.UK.sectionsDao().clearAll()
-            BaseCodexDatabase.UK.partsDao().clearAll()
-
-            BaseCodexDatabase.UPK.articlesDao().clearAll()
-            BaseCodexDatabase.UPK.chaptersDao().clearAll()
-            BaseCodexDatabase.UPK.sectionsDao().clearAll()
-            BaseCodexDatabase.UPK.partsDao().clearAll()
-
-            BaseCodexDatabase.KoAP.articlesDao().clearAll()
-            BaseCodexDatabase.KoAP.chaptersDao().clearAll()
-            BaseCodexDatabase.KoAP.sectionsDao().clearAll()
-            BaseCodexDatabase.KoAP.partsDao().clearAll()
-
-            BaseCodexDatabase.PIKoAP.articlesDao().clearAll()
-            BaseCodexDatabase.PIKoAP.chaptersDao().clearAll()
-            BaseCodexDatabase.PIKoAP.sectionsDao().clearAll()
-            BaseCodexDatabase.PIKoAP.partsDao().clearAll()
+            clearDatabase(BaseCodexDatabase.UK)
+            clearDatabase(BaseCodexDatabase.UPK)
+            clearDatabase(BaseCodexDatabase.KoAP)
+            clearDatabase(BaseCodexDatabase.PIKoAP)
 
             BaseCodexProvider.update()
 
             Snackbar.make(requireView(), "Базы данных очищены", Snackbar.LENGTH_SHORT).show()
         }
+    }
+
+    // debug function
+    private fun clearDatabase(database: CodexDatabase){
+        database.articlesDao().clearAll()
+        database.chaptersDao().clearAll()
+        database.sectionsDao().clearAll()
+        database.partsDao().clearAll()
     }
 
     override fun onDestroyView() {
