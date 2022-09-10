fee618a39471b7aaf6ede0eff1cf1a92e40d9abe
tokyolem <heenssss@mail.ru>
2022-09-10 23:27:53 +0300

    feat: class to check network status added

--- app/src/main/java/com/team/lawsrb/basic/NetworkAvailable.kt
+++ app/src/main/java/com/team/lawsrb/basic/NetworkAvailable.kt
@@ -1,26 +1,84 @@
 package com.team.lawsrb.basic
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
 import java.io.IOException
 import java.net.InetSocketAddress
 import java.net.Socket
 import java.net.SocketAddress
 
-object NetConnection {
+class NetworkAvailable(context: Context) {
+    companion object {
+        var isNetworkAvailable: Boolean = false
+    }
+    private val TAG = "NetworkAvailable"
+    private val connectivityManager = getSystemService(context, ConnectivityManager::class.java) as ConnectivityManager
+    private val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+        .build()
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        // network is available for use
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            isNetworkAvailable = checkInternetConnection()
+            Log.d(TAG, "Net $isNetworkAvailable")
+            Log.d(TAG, "Network turned on")
+        }
 
-    private const val TAG = "NetworkAvailable"
-    fun isOnline(): Boolean {
+        // Network capabilities have changed for the network
+        override fun onCapabilitiesChanged(
+            network: Network,
+            networkCapabilities: NetworkCapabilities
+        ) {
+            super.onCapabilitiesChanged(network, networkCapabilities)
+            val isChanges = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
+            Log.d(TAG, "$isChanges")
+        }
+
+        // lost network connection
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            isNetworkAvailable = false
+            Log.d(TAG, "Net $isNetworkAvailable")
+            Log.d(TAG, "Network turned off")
+        }
+    }
+
+    fun mRegisterNetworkCallback() {
+        try {
+            connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+        }
+        catch (e: IOException) {
+            Log.e(TAG, "${e.message}")
+        }
+    }
+
+    fun mUnregisterNetworkCallback() {
+        try {
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        }
+        catch (e: IOException) {
+            Log.e(TAG, "${e.message}")
+        }
+    }
+
+    private fun checkInternetConnection(): Boolean {
         return try {
             val timeoutMs = 1500
             val socket = Socket()
             val socketAddr: SocketAddress = InetSocketAddress("8.8.8.8", 53)
             socket.connect(socketAddr, timeoutMs)
             socket.close()
-            Log.i(TAG, "The device has internet access")
             true
         }
         catch (e: IOException) {
-            Log.d(TAG, "No internet access")
             false
         }
     }
