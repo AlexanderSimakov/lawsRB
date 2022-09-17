f742a7946b4ad2e0786c065bd05a71b4452e0f0e
Alexander <casha4367@mail.ru>
2022-09-17 23:03:37 +0300

    feat(PageNavigation): add adjust page function

--- app/src/main/java/com/team/lawsrb/ui/codexPageFragments/PageNavigation.kt
+++ app/src/main/java/com/team/lawsrb/ui/codexPageFragments/PageNavigation.kt
@@ -1,7 +1,9 @@
 package com.team.lawsrb.ui.codexPageFragments
 
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.team.lawsrb.basic.dataProviders.CodexProvider
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -12,9 +14,30 @@ object PageNavigation {
     private var recyclers: MutableList<Recycler> = mutableListOf()
     private val delayBeforeScrolling = 300L
 
+    val currentPage: Page
+        get() {
+            return when(viewPager!!.currentItem){
+                0 -> Page.SECTIONS
+                1 -> Page.CHAPTERS
+                2 -> Page.ARTICLES
+                else -> throw IllegalArgumentException("viewPager item was ${viewPager!!.currentItem}, should be in [0..2]")
+            }
+        }
+
+    enum class Page(val itemIndex: Int) {
+        SECTIONS(0),
+        CHAPTERS(1),
+        ARTICLES(2)
+    }
+
     fun addRecyclerView(view: RecyclerView, items: List<Any>, number: Int) {
         recyclers.add(number, Recycler(view, items))
     }
+
+    fun moveTo(page: Page){
+        viewPager!!.setCurrentItem(page.itemIndex, true)
+    }
+
 
     fun moveLeftTo(codexObject: Any) {
         viewPager!!.setCurrentItem(viewPager!!.currentItem - 1, true)
@@ -34,6 +57,90 @@ object PageNavigation {
         recyclers[viewPager!!.currentItem].let { recycler ->
             val position = recycler.items.indexOfFirst { it == codexObject }
             recycler.recyclerView.smoothScrollToPosition(position)
+        }
+    }
+
+    fun adjustCurrentPageByItems(codeProvider: CodexProvider){
+        /*
+            s - section, c - chapter, a - Article (S/C/A - current page)
+            0 - empty page
+            1 - not empty page
+
+            All variations:
+            S|c|a  s|C|a  s|c|A
+            0|0|0  0|0|0  0|0|0
+            0|0|1  0|0|1  0|0|1
+            0|1|0  0|1|0  0|1|0
+            0|1|1  0|1|1  0|1|1
+            1|0|0  1|0|0  1|0|0
+            1|0|1  1|0|1  1|0|1
+            1|1|0  1|1|0  1|1|0
+            1|1|1  1|1|1  1|1|1
+
+            (1) if current page is not empty, then stay on this page
+                  Before:
+            S|c|a  s|C|a  s|c|A
+            0|0|0  0|0|0  0|0|0
+            0|0|1  0|0|1  0|1|0
+            0|1|0  1|0|0  1|0|0
+            0|1|1  1|0|1  1|1|0
+
+            (2) if all pages are empty, then stay on current page
+                  Before:
+            S|c|a  s|C|a  s|c|A
+            0|0|1  0|0|1  0|1|0
+            0|1|0  1|0|0  1|0|0
+            0|1|1  1|0|1  1|1|0
+
+            (3) if only one page is not empty, then choose that page
+                  Before:
+            S|c|a  s|C|a  s|c|A
+            0|1|1  1|0|1  1|1|0
+         */
+
+        // (1) if current page is not empty, then stay on this page
+        if (currentPage == Page.SECTIONS &&
+            codeProvider.isSectionPageItemsNotEmpty ||
+            currentPage == Page.CHAPTERS &&
+            codeProvider.isChapterPageItemsNotEmpty ||
+            currentPage == Page.ARTICLES &&
+            codeProvider.isArticlePageItemsNotEmpty){
+            return
+        }
+
+        // (2) if all pages are empty, then stay on current page
+        if (codeProvider.isSectionPageItemsEmpty &&
+            codeProvider.isChapterPageItemsEmpty &&
+            codeProvider.isArticlePageItemsEmpty){
+            return
+        }
+
+        // (3) if only one page is not empty, then choose those page
+        if (codeProvider.isSectionPageItemsNotEmpty &&
+            codeProvider.isChapterPageItemsEmpty &&
+            codeProvider.isArticlePageItemsEmpty){
+            moveTo(Page.SECTIONS)
+        }
+        else if (codeProvider.isSectionPageItemsEmpty &&
+            codeProvider.isChapterPageItemsNotEmpty &&
+            codeProvider.isArticlePageItemsEmpty){
+            moveTo(Page.CHAPTERS)
+        }
+        else if (codeProvider.isSectionPageItemsEmpty &&
+            codeProvider.isChapterPageItemsEmpty &&
+            codeProvider.isArticlePageItemsNotEmpty){
+            moveTo(Page.ARTICLES)
+        }
+
+        // (4) solve last variations
+        else if (currentPage == Page.SECTIONS){
+            moveTo(Page.CHAPTERS)
+        }
+        else if (currentPage == Page.CHAPTERS){
+            moveTo(Page.ARTICLES)
+        }
+        else if (currentPage == Page.ARTICLES){
+            moveTo(Page.CHAPTERS)
         }
     }
 
