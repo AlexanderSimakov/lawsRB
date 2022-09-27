0a6c8818867bb02f0d63c1344e727bf75d967c94
tokyolem <heenssss@mail.ru>
2022-09-27 22:57:20 +0300

    refact: unused test logs removed

--- app/src/main/java/com/requestfordinner/lawsrb/ui/codexFragments/CodexKoAPFragment.kt
+++ app/src/main/java/com/requestfordinner/lawsrb/ui/codexFragments/CodexKoAPFragment.kt
@@ -1,7 +1,6 @@
 package com.requestfordinner.lawsrb.ui.codexFragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -59,7 +58,6 @@ class CodexKoAPFragment : Fragment() {
         }.attach()
 
         currentTabLayout = tabLayout
-        Log.d("TEST", "${currentTabLayout}")
     }
 
     override fun onStart() {
