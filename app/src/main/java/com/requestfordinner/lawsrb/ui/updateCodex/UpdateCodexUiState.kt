4b17cd8bb9494b44cc285782ee3b21feb0ac310d
Alexander <casha4367@mail.ru>
2022-11-11 13:07:24 +0300

    feat(update page): add ui state data class

--- app/src/main/java/com/requestfordinner/lawsrb/ui/updateCodex/UpdateCodexUiState.kt
+++ app/src/main/java/com/requestfordinner/lawsrb/ui/updateCodex/UpdateCodexUiState.kt
@@ -1,4 +1,69 @@
 package com.requestfordinner.lawsrb.ui.updateCodex
 
-class UpdateCodexUiState {
+import com.requestfordinner.lawsrb.basic.htmlParser.Codex
+
+/**
+ *
+ */
+data class UpdateCodexUiState(
+    /** */
+    val UKButtonState: ButtonState = ButtonState.DISABLED,
+    /** */
+    val UPKButtonState: ButtonState = ButtonState.DISABLED,
+    /** */
+    val KoAPButtonState: ButtonState = ButtonState.DISABLED,
+    /** */
+    val PIKoAPButtonState: ButtonState = ButtonState.DISABLED,
+    /**  */
+    val checkUpdatesButtonState: ButtonState = ButtonState.ENABLED,
+
+    /** */
+    var messageToShow: Int? = null
+) {
+
+    /** */
+    enum class ButtonState {
+        /** */
+        DISABLED,
+
+        /** */
+        ENABLED,
+
+        /** */
+        UPDATING
+    }
+
+    fun isUpdateEnabled(): Boolean {
+        return UKButtonState == ButtonState.ENABLED ||
+                UPKButtonState == ButtonState.ENABLED ||
+                KoAPButtonState == ButtonState.ENABLED ||
+                PIKoAPButtonState == ButtonState.ENABLED
+    }
+
+    /** */
+    fun getState(codex: Codex): ButtonState {
+        return when (codex) {
+            Codex.UK -> UKButtonState
+            Codex.UPK -> UPKButtonState
+            Codex.KoAP -> KoAPButtonState
+            Codex.PIKoAP -> PIKoAPButtonState
+        }
+    }
+
+    /** */
+    fun copy(
+        states: List<Pair<Codex, ButtonState>>,
+        messageToShow: Int? = this.messageToShow
+    ): UpdateCodexUiState {
+        return UpdateCodexUiState(
+            UKButtonState = states.get(Codex.UK),
+            UPKButtonState = states.get(Codex.UPK),
+            KoAPButtonState = states.get(Codex.KoAP),
+            PIKoAPButtonState = states.get(Codex.PIKoAP),
+            messageToShow = messageToShow
+        )
+    }
+
+    private fun List<Pair<Codex, ButtonState>>.get(codex: Codex): ButtonState =
+        this.find { it.first == codex }?.second ?: getState(codex)
 }
