5b03dff6ade8da3deca6b101b52fdd57787e269c
AlexanderSimakov <50450591+AlexanderSimakov@users.noreply.github.com>
2023-04-16 01:31:01 +0300

    Create empty flutter project (#165)

--- app/src/main/java/com/requestfordinner/lawsrb/basic/roomDatabase/codexObjects/TitledItem.kt
+++
@@ -1,7 +0,0 @@
-package com.requestfordinner.lawsrb.basic.roomDatabase.codexObjects
-
-/** Mark that something has title. */
-interface TitledItem {
-    /** The [title] of something. */
-    val title: String
-}
